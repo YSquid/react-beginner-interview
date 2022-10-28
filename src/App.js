0dcb1c2d4da2003a708cc1597b1b65a002ce5005
Ysquid <ahk648@gmail.com>
2022-10-28 12:29:13 -0600

    moved data fetch into async

--- src/App.js
+++ src/App.js
@@ -3,29 +3,39 @@ import "./App.css";
 
 function App() {
 
-  const [randomUser, setRandomUser] = useState(null)
+  const [randomUser, setRandomUser] = useState('')
   const [users, setUsers] = useState([])
+  const [userCount, setUserCount] = useState(0)
 
   const endpoint = "https://randomuser.me/api";
 
-  const getRandomUser = async () => {
-    const response = await fetch(endpoint)
-    console.log(response)
-    const data = await response.json()
-    console.log(data)
-    setRandomUser(data)
-  }
+  useEffect(() => {
+    const getRandomUser = async () => {
+      const response = await fetch(endpoint)
+      console.log(response)
+      const data = await response.json()
+      console.log(data.results)
+      setRandomUser(data.results)
+    }
+
+    getRandomUser()
+  }, [])
+  
 
   const saveUser = () => {
     setUsers([...users, randomUser])
   }
+
+  // const getRandomUserFullName = () => {
+  //   return `First: ${randomUser[0].name.first} Last: ${randomUser[0].name.first}`
+  // }
   
 
   return (
     <div className="App">
-      <button onClick={getRandomUser}>Get Random User</button>
       <h1>Random User</h1>
       <p>{JSON.stringify(randomUser)}</p>
+      {/* <p>{getRandomUserFullName()}</p> */}
       <button onClick={saveUser}>Save Random User</button>
     </div>
   );
