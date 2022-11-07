7f64f11816ea7369217e4911bd5f66c55910b179
Ysquid <ahk648@gmail.com>
2022-11-07 13:44:14 -0600

    randomUser module working on load

--- src/RandomUser.js
+++ src/RandomUser.js
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from "react";
 
 function RandomUser() {
   const [randomUser, setRandomUser] = useState("");
-
+  
+  const getRandomUser = async () => {
+    const endpoint = "https://randomuser.me/api";
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    setRandomUser(data.results[0]);
+  };
+  
   useEffect(() => {
-    const getRandomUser = async () => {
-      const endpoint = "https://randomuser.me/api";
-      const response = await fetch(endpoint);
-      const data = await response.json();
-      setRandomUser(data.results);
-    };
     getRandomUser();
   }, []);
 
   return (
-  <p>{randomUser.gender}</p>
+    <>
+      <p>
+        {randomUser ? `${randomUser.name.first} ${randomUser.name.last}` : null}
+      </p>
+      <img src={randomUser ? randomUser.picture.thumbnail : null} />
+    </>
   );
 }
 export default RandomUser;
